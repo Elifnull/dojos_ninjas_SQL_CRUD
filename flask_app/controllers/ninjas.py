2274de94caa068397c2630dc05f42839d5c521e2
Serge Chvorun <elifnull@protonmail.com>
2022-01-23 18:04:46 -0500

    insert function error fixed

--- flask_app/controllers/ninjas.py
+++ flask_app/controllers/ninjas.py
@@ -1,13 +1,14 @@
 from flask import render_template, redirect, request
 from flask_app import app
-from flask_app.models import ninja, dojo
+from flask_app.models.ninja import Ninja
+from flask_app.models.dojo import Dojo
 
 @app.route("/ninjas")
 def ninjas():
-    dojos = dojo.Dojo.get_all()
+    dojos = Dojo.get_all()
     return render_template("ninjas.html", all_dojos = dojos)
 
-@app.route("/add_ninjas", methods=["POST"])
+@app.route("/add_ninja", methods=["POST"])
 def add_ninjas():
-    ninja.Ninja.save(request.form)
-    return redirect("/")
+    Ninja.save(request.form)
+    return redirect("/dojos")
